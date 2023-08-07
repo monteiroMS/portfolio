28543dafe046ce6839d228fa15b0dc1e6212bb55
Matheus Monteiro <matheuschran@hotmail.com>
2023-08-07 15:17:27 -0300

    feat: add new pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
-import { Route } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import { Switch } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Technologies from "./pages/Technologies";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/projects" component={ Projects } />
-      <Route path="/contact" component={ Contact } />
-      <Route path="/404" component={ NotFound } />
-      <Redirect to="/404" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/projetos" component={Projects} />
+        <Route path="/tecnologias" component={Technologies} />
+        <Route path="/contato" component={Contact} />
+        <Route path="/404" component={NotFound} />
+        <Redirect to="/404" />
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
